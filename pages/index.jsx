f6b2a9cf238cc90566582cdc64bbc0056fd9c99a
Ramsi88 <ramsi88@me.com>
2024-01-01 15:44:57 +0100

    Removed and fixed web3 import

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
-import {Web3} from "web3";
+//import {Web3} from "web3";
 
 //FACTORY_ADDRESS,
 import { FACTORY_ABI, CONTRACT_ABI, CONTRACT_ADDRESS } from "../constants";
