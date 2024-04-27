43eca355544f7c1d91f446da943eb8770f1818d6
rms <0xrms@proton.me>
2024-04-28 01:42:15 +0200

    L2 Polygon deploy

--- pages/index.jsx
+++ pages/index.jsx
@@ -171,10 +171,10 @@ const actions = [
 //{block,price,gas,usdBalance}
 export default function Home() {
   const networks = {
-    mainnet : {id: 1, name: "Ethereum Mainnet", contract:"0x91b3920e3F9813ded3AC0A7bF24D8a97352ac8C2"},
+    mainnet : {id: 1, name: "Mainnet", contract:"0x91b3920e3F9813ded3AC0A7bF24D8a97352ac8C2"},
     goerli: {id: 5, name: "Goerli", contract:"0x6Fb80eD4Dc22307Fc54851d6f051399ac1357A1f"},
     sepolia: {id: 11155111, name: "Sepolia", contract:"0x1f4f156f079a0E6e55d5687c3f32B575232d036E"},
-    //polygon: {id: 137, name: "Polygon", contract:""},
+    polygon: {id: 137, name: "Polygon", contract:"0x91b3920e3F9813ded3AC0A7bF24D8a97352ac8C2"},
     mumbai: {id: 80001, name: "Mumbai", contract:"0x2139EE209aC63471E2Bb522Af904C84c66e33f88"},
     amoy: {id: 80002, name: "Amoy", contract: "0xE3f25232475D719DD89FF876606141308701B713"},
     //base: {id: 8453, name: "Base", contract:""},
